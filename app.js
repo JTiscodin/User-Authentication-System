0cffef34a1281a3f1c06b2f7cc48dc551c7b2f83
JTiscodin <you@example.com>
2023-06-29 21:25:08 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 
+
 const express = require("express")
 
 const mongoose = require("mongoose")
+
+const session = require("express-session")
+
+const passport = require("passport")
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const ejs = require("ejs")
 
@@ -11,8 +20,11 @@ const bodyParser = require("body-parser")
 const app = express()
 
 const encrypt = require("mongoose-encryption")
+const { compareSync } = require('bcrypt')
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB")
+const findOrCreate = require("mongoose-findorcreate")
+
+
 
 app.set("view engine", "ejs")
 
@@ -20,23 +32,66 @@ app.use(express.static("public"))
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-console.log(process.env.API_KEY)
+app.use(session({
+    secret: "Just some secrets",
+    resave: false,
+    saveUninitialized: false,
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB")
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secrets: String
 })
 
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]})
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]})
 
 const User = mongoose.model("User", userSchema)
 
+passport.use(User.createStrategy())
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", (req,res) => {
     res.render("home")
+})
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/secrets", 
+    passport.authenticate("google",{ failureRedirect:"/login" }),function(req,res)  {
+    res.redirect("/secrets")
 })
 
 app.get("/login", (req,res) => {
@@ -47,23 +102,58 @@ app.get("/register", (req,res) => {
     res.render("register")
 })
 
-app.post("/register", (req,res) => {
-    User.create({email: req.body.username, password: req.body.password}).then((cre) => {
-        res.render("secrets")
+app.get("/secrets", (req,res) => {
+    User.find({"secrets" : {$ne: null}}).then((found) => {
+        res.render("secrets", {usersecrets: found})
     })
 })
 
-app.post("/login", (req,res) => {
-    const user = req.body.username
-    const password = req.body.password
+app.get("/submit",(req,res) => {
+    if(req.isAuthenticated()){
+        res.render("submit")
+       
+    }else{
+     
+        res.redirect("/login")
+    }
+})
 
-    User.findOne({email: user}).then((found) => {
-        if(found.password === password){
-            res.render("secrets")
+app.post("/register", (req,res) => {
+    
+    User.register({username: req.body.username} , req.body.password, function(err,user) {
+        if(err){
+            console.log(err)
+            res.redirect("/register")
+        }else {
+            passport.authenticate("local")(req,res,() =>{
+                res.redirect("/secrets")
+            })
+                
+            }
+        })
+    })
+
+app.post("/login", passport.authenticate("local"), function(req,res) {
+    res.redirect("/secrets")
+})
+  
+app.get("/logout" ,(req,res) => {
+    req.logOut((err) => {
+        if(err){
+            return next(err)
         }else{
-            console.log("Wrong credentials")
-            res.redirect("login")
+            res.redirect("/")
         }
+    })
+})
+
+
+app.post("/submit", (req,res) => {
+    const submittedSecret = req.body.secret
+    User.findById(req.user.id).then((found) => {
+        found.secrets = submittedSecret
+        found.save()
+        res.redirect("/secrets")
     })
 })
 
